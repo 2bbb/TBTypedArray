b23e187971887c4609cbbc221837c98a0e29033b
ISHII 2bit <ishii@buffer-renaiss.com>
2012-08-06 18:25:46 +0900

    Initial Commit

---
+++ TBTypedArrayExample/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  TBTypedArrayExample
+//
+//  Created by 通人 石井 on 12/08/06.
+//  Copyright (c) 2012年 buffer Renaiss. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
